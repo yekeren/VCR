696ec8450d0ff52101bce22b8773946b3ab54bd9
yekeren <yekeren.cn@gmail.com>
2020-01-31 13:42:45 -0500

    r2c

--- vcr/r2c_frozen.py
+++ vcr/r2c_frozen.py
@@ -99,7 +99,9 @@ def _assign_invalid_tags(tags, max_num_objects):
 def _get_class_embedding_vectors(label,
                                  vocab_file,
                                  vocab_size,
-                                 embedding_dims=300):
+                                 embedding_dims=300,
+                                 scope='object_embedding',
+                                 max_norm=None):
   """Gets token embedding vectors.
 
   Args:
@@ -113,11 +115,11 @@ def _get_class_embedding_vectors(label,
   """
   label_ids = token_to_id.TokenToIdLayer(vocab_file,
                                          unk_token_id=vocab_size)(label)
-  with tf.variable_scope('object_embedding'):
+  with tf.variable_scope(scope):
     object_embedding = tf.get_variable('weights',
                                        shape=[vocab_size + 1, embedding_dims],
                                        trainable=True)
-  return tf.nn.embedding_lookup(object_embedding, label_ids)
+  return tf.nn.embedding_lookup(object_embedding, label_ids, max_norm=max_norm)
 
 
 def _project_object_features(object_features,
@@ -239,6 +241,124 @@ class VCRR2CFrozen(ModelBase):
     with slim.arg_scope(fc_scope_fn()):
       return self._predict(inputs, **kwargs)
 
+  def _recognition_to_cognition(self, question_inp_features, question_len,
+                                answer_inp_features, answer_len,
+                                object_features, num_objects, predictions):
+    """Creates the `RecognitionToCognition` network.
+
+    Args:
+      question_inp_features: Input question features, a [batch*NUM_CHOICES,
+        max_question_len, feature_dims] float tensor.
+      question_len: Question length, a [batch*NUM_CHOICES] int tensor.
+      answer_inp_features: Input answer features, a [batch*NUM_CHOICES,
+        max_answer_len , feature_dims] float tensor.
+      answer_len: Answer length, a [batch*NUM_CHOICES] int tensor.
+      object_features: Object features, a [batch, max_num_objects, object_dims]
+        float tensor.
+      num_objects: A [batch] int tensor.
+
+    Returns:
+      final_features: A [batch, output_dims] float tensor.
+      answer_seq_features: Contextualized answer features, a [batch*NUM_CHOICES,
+        max_answer_len , feature_dims] float tensor.
+    """
+    is_training = self._is_training
+    options = self._model_proto
+
+    (question_max_len, answer_max_len) = (tf.shape(question_inp_features)[1],
+                                          tf.shape(answer_inp_features)[1])
+    batch_size = object_features.shape[0]
+    max_num_objects = tf.shape(object_features)[1]
+
+    # Encode the sequence using BiLSTM model.
+    with tf.variable_scope('grounding_encoder'):
+      question_seq_features, _ = rnn.RNN(question_inp_features,
+                                         question_len,
+                                         options.rnn_config,
+                                         is_training=is_training)
+
+    with tf.variable_scope('grounding_encoder', reuse=True):
+      answer_seq_features, _ = rnn.RNN(answer_inp_features,
+                                       answer_len,
+                                       options.rnn_config,
+                                       is_training=is_training)
+
+    # Get the question features attended by the answers.
+    #   qa_mask: [batch*NUM_CHOICES, question_max_len, 1].
+    #   qa_similarity: [batch*NUM_CHOICES, question_max_len, answer_max_len].
+    #   qa_attention_weights: [batch*NUM_CHOICES, question_max_len, answer_max_len].
+    #   attended_question: [batch*NUM_CHOICES, answer_max_len, feature_dims].
+    qa_mask = tf.expand_dims(
+        tf.sequence_mask(question_len, question_max_len, dtype=tf.float32), 2)
+    with tf.variable_scope('qa_bilinear'):
+      qa_similarity = attention_ops.bilinear(question_seq_features,
+                                             answer_seq_features)
+    qa_attention_weights = masked_ops.masked_softmax(data=qa_similarity,
+                                                     mask=qa_mask,
+                                                     dim=1)
+    attended_question = tf.einsum('bqa,bqd->bad', qa_attention_weights,
+                                  question_seq_features)
+
+    # Attention over the objects.
+    #   oa_mask: [batch, max_num_object, 1].
+    #   oa_similarity: [batch*NUM_CHOICES, max_num_object, answer_max_len]
+    #   oa_attention_weights: [batch*NUM_CHOICES, max_num_object, answer_max_len].
+    #   attended_objects: [batch*NUM_CHOICES, answer_max_len, object_dims].
+
+    tile_fn = lambda x: tf.gather(tf.expand_dims(x, 1), [0] * NUM_CHOICES,
+                                  axis=1)
+    object_features = tf.reshape(
+        tile_fn(object_features),
+        [batch_size * NUM_CHOICES, -1, object_features.shape[-1]])
+    num_objects = tf.reshape(tile_fn(num_objects), [-1])
+
+    oa_mask = tf.expand_dims(
+        tf.sequence_mask(num_objects, max_num_objects, dtype=tf.float32), 2)
+    with tf.variable_scope('oa_bilinear'):
+      oa_similarity = attention_ops.bilinear(object_features,
+                                             answer_seq_features)
+    oa_attention_weights = masked_ops.masked_softmax(data=oa_similarity,
+                                                     mask=oa_mask,
+                                                     dim=1)
+    attended_objects = tf.einsum('boa,bod->bad', oa_attention_weights,
+                                 object_features)
+
+    # Reasoning module.
+    reasoning_inp_features = tf.concat(
+        [answer_seq_features, attended_question, attended_objects], -1)
+
+    with tf.variable_scope('reasoning'):
+      reasoning_seq_features, _ = rnn.RNN(reasoning_inp_features,
+                                          answer_len,
+                                          options.rnn_config,
+                                          is_training=is_training)
+
+    # Pool features from the sequence.
+    pooling_fn = (masked_ops.masked_max_nd
+                  if options.use_max_pooling else masked_ops.masked_avg_nd)
+
+    final_seq_features = tf.concat([
+        reasoning_seq_features, answer_seq_features, attended_question,
+        attended_objects
+    ], -1)
+    final_features = pooling_fn(data=final_seq_features,
+                                mask=tf.sequence_mask(answer_len,
+                                                      answer_max_len,
+                                                      dtype=tf.float32),
+                                dim=1)
+
+    # Export summaries.
+    tf.compat.v1.summary.histogram('attention/qa_similarity', qa_similarity)
+    tf.compat.v1.summary.histogram('attention/oa_similarity', oa_similarity)
+    predictions.update({
+        'r2c/num_objects': num_objects,
+        'r2c/oa_mask': oa_mask,
+        'r2c/qa_mask': qa_mask,
+        'r2c/oa_attention_weights': oa_attention_weights,
+        'r2c/qa_attention_weights': qa_attention_weights,
+    })
+    return (tf.squeeze(final_features, 1), answer_seq_features)
+
   def _predict(self, inputs, **kwargs):
     """Predicts the resulting tensors.
 
@@ -250,6 +370,8 @@ class VCRR2CFrozen(ModelBase):
     """
     is_training = self._is_training
     options = self._model_proto
+
+    predictions = {}
 
     # Extract text annotations.
     (question_embs, question_len, answer_embs, answer_len,
@@ -298,106 +420,14 @@ class VCRR2CFrozen(ModelBase):
         object_features, question_tags)
     answer_object_features = _ground_tag_using_object_features(
         object_features, answer_tags)
-
-    # Encode the sequence using BiLSTM model.
-    question_rnn_inputs, answer_rnn_inputs = question_embs, answer_embs
     question_rnn_inputs = tf.concat([question_embs, question_object_features],
                                     -1)
     answer_rnn_inputs = tf.concat([answer_embs, answer_object_features], -1)
 
-    with tf.variable_scope('grounding_encoder'):
-      question_seq_features, _ = rnn.RNN(question_rnn_inputs,
-                                         question_len,
-                                         options.rnn_config,
-                                         is_training=is_training)
-
-    with tf.variable_scope('grounding_encoder', reuse=True):
-      answer_seq_features, _ = rnn.RNN(answer_rnn_inputs,
-                                       answer_len,
-                                       options.rnn_config,
-                                       is_training=is_training)
-
-    # Attention over the question.
-    #   qa_similarity: [batch*NUM_CHOICES, question_len, answer_len].
-    #   qa_mask: [batch*NUM_CHOICES, question_len].
-    #   qa_attention_weights: [batch*NUM_CHOICES, question_len, answer_len].
-    #   question_seq_features: [batch*NUM_CHOICES, question_len, dims].
-    #   attended_question: [batch*NUM_CHOICES, answer_len, dims].
-    with tf.variable_scope('qa_bilinear'):
-      qa_similarity = attention_ops.bilinear(question_seq_features,
-                                             answer_seq_features)
-      tf.compat.v1.summary.histogram('attention/qa_similarity', qa_similarity)
-    qa_mask = tf.expand_dims(
-        tf.sequence_mask(question_len,
-                         tf.shape(question_seq_features)[1],
-                         dtype=tf.float32), 2)
-    qa_attention_weights = masked_ops.masked_softmax(data=qa_similarity,
-                                                     mask=qa_mask,
-                                                     dim=1)
-    # attended_question = tf.reduce_sum(
-    #     tf.multiply(tf.expand_dims(qa_attention_weights, 3),
-    #                 tf.expand_dims(question_seq_features, 2)), 1)
-    attended_question = tf.einsum('bqa,bqd->bad', qa_attention_weights,
-                                  question_seq_features)
-
-    # Attention over the objects.
-    #   oa_similarity: [batch*NUM_CHOICES, object_len, answer_len]
-    #   oa_mask: [batch*NUM_CHOICES, object_len].
-    #   oa_attention_weights: [batch*NUM_CHOICES, object_len, answer_len].
-    #   attended_objects: [batch*NUM_CHOICES, answer_len, dims].
-
-    object_features = tf.gather(tf.expand_dims(object_features, 1),
-                                [0] * NUM_CHOICES,
-                                axis=1)
-    object_features = tf.reshape(
-        object_features,
-        [batch_size * NUM_CHOICES, -1, object_features.shape[-1]])
-    num_objects = tf.tile(num_objects, [NUM_CHOICES])
-
-    with tf.variable_scope('oa_bilinear'):
-      oa_similarity = attention_ops.bilinear(object_features,
-                                             answer_seq_features)
-      tf.compat.v1.summary.histogram('attention/oa_similarity', oa_similarity)
-    oa_mask = tf.expand_dims(
-        tf.sequence_mask(num_objects,
-                         tf.shape(object_features)[1],
-                         dtype=tf.float32), 2)
-    oa_attention_weights = masked_ops.masked_softmax(data=oa_similarity,
-                                                     mask=oa_mask,
-                                                     dim=1)
-    # attended_objects = tf.reduce_sum(
-    #     tf.multiply(tf.expand_dims(oa_attention_weights, 3),
-    #                 tf.expand_dims(object_features, 2)), 1)
-    attended_objects = tf.einsum('boa,bod->bad', oa_attention_weights,
-                                 object_features)
-
-    # Reasoning.
-    reasoning_rnn_input = tf.concat(
-        [answer_seq_features, attended_question, attended_objects], -1)
-
-    with tf.variable_scope('reasoning'):
-      reasoning_seq_features, _ = rnn.RNN(reasoning_rnn_input,
-                                          answer_len,
-                                          options.rnn_config,
-                                          is_training=is_training)
-
-    # Pool features from the sequence.
-    final_seq_features = tf.concat(
-        [reasoning_seq_features, answer_seq_features, attended_question], -1)
-
-    if options.use_max_pooling:
-      pooling_fn = masked_ops.masked_max_nd
-    else:
-      pooling_fn = masked_ops.masked_avg_nd
-
-    final_features = pooling_fn(data=final_seq_features,
-                                mask=tf.sequence_mask(
-                                    answer_len,
-                                    tf.shape(final_seq_features)[1],
-                                    dtype=tf.float32),
-                                dim=1)
-
-    final_features = tf.squeeze(final_features, 1)
+    # Build the recognition to cognition model.
+    final_features, answer_seq_features = self._recognition_to_cognition(
+        question_rnn_inputs, question_len, answer_rnn_inputs, answer_len,
+        object_features, num_objects, predictions)
 
     if options.use_cls_token_embedding:
       cls_bert = tf.reshape(inputs[InputFields.cls_bert],
@@ -419,7 +449,12 @@ class VCRR2CFrozen(ModelBase):
                                                    activation_fn=None)
 
     output = tf.reshape(output, [batch_size, NUM_CHOICES])
-    return {FIELD_ANSWER_PREDICTION: output}
+
+    predictions.update({
+        FIELD_ANSWER_PREDICTION: output,
+        'image_id': inputs[InputFields.img_id]
+    })
+    return predictions
 
   def build_losses(self, inputs, predictions, **kwargs):
     """Computes loss tensors.
